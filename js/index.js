51075b458cacee77ce2a294d3a3e93571ab24522
henghenghahadada <xing19931122@sina.com>
2017-05-24 19:23:39 +0800

    transition

--- js/index.js
+++ js/index.js
@@ -3,10 +3,7 @@ $(function(){
 		$('.btnbox a').removeClass('on1').eq($(this).index()).addClass('on1');
 		$('.showbox ul').hide().eq($(this).index()).show()
 	});
-	$('.fx').toggle(function(){
-		$('.bdsharebuttonbox').show()
-	});
-	$('.b_p1').animate({marginTop:'2rem'});
-	$('.banner h1').animate({marginTop:'1.15rem'});
-	$('.b_p2').animate({marginTop:'1.15rem'});
+	$('.b_p1').animate({marginTop:'2rem', transition:'2s'});
+	$('.banner h1').animate({marginTop:'1.15rem',transition:'2s'});
+	$('.b_p2').animate({marginTop:'1.15rem',transition:'2s'});
 })
